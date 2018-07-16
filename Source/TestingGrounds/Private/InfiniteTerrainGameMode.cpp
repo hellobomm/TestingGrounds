7e9ef9d478760b036b9b0ae7b5181d03d0f722c4
Alex-PC/Alex <hellobomm@web.de>
2018-07-16 19:07:10 +0200

    Lecture317

---
+++ Source/TestingGrounds/Private/InfiniteTerrainGameMode.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InfiniteTerrainGameMode.h"
+#include "AI/Navigation/NavMeshBoundsVolume.h"
+#include "EngineUtils.h"
+
+
+
+void AInfiniteTerrainGameMode::PopulateBoundsVolumePool()
+{
+	//get all the ANavMeshBoundsVolume Actors of the world and put it in an Iterator
+	TActorIterator<ANavMeshBoundsVolume> NavMeshBoundsVolumeIterator = TActorIterator<ANavMeshBoundsVolume>(GetWorld());
+	while (NavMeshBoundsVolumeIterator)  //that is how you write this. It looks weird
+	{
+		AddToPool(*NavMeshBoundsVolumeIterator); //the content is of course the pointer to a NavMeshBoundsVolume
+		ANavMeshBoundsVolume* FoundNavMeshBoundsVolume = *NavMeshBoundsVolumeIterator;
+		UE_LOG(LogTemp,Warning,TEXT("Found FoundNavMeshBoundsVolume: %s"), *FoundNavMeshBoundsVolume->GetName())
+		++NavMeshBoundsVolumeIterator;  //otherwise it won't compile
+	}
+}
+
+void AInfiniteTerrainGameMode::AddToPool(ANavMeshBoundsVolume * VolumeToAdd)
+{
+
+}
